89f6fdc2987fa3a041a366e944add88dd9132ca0
Ridho Tadjudin <ridhotadjudin@gmail.com>
2022-06-16 14:40:33 +0700

    update ridho

--- src/test/java/com/nextsoft/testcom/product/BuyProductTest.java
+++ src/test/java/com/nextsoft/testcom/product/BuyProductTest.java
@@ -1,10 +1,5 @@
 package com.nextsoft.testcom.product;
 
 public class BuyProductTest {
-<<<<<<< HEAD
-	//test produk
-=======
-	//test titan
-
->>>>>>> 5c937e78879166258ec70de862283184c72d10a5
+	//test ridho
 }
